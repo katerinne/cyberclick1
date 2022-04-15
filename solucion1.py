a680445029ae8e83a5bdcb9788be4ef9082b5659
Belkis <katerinne65@gmail.com>
2022-04-15 16:59:09 +0200

    Metodo lista

--- solucion1.py
+++ solucion1.py
@@ -1,12 +1,25 @@
 resp = "0"
 
+def lista():  
+    lista = []      
+    for i in range (1,101):          
+        palabra = ""
+        if( i % 3 == 0):             
+            palabra='cyber'
+        if( i % 5 == 0):
+            palabra+='click'                       
+        if(palabra == ""):
+            palabra = i
+        lista.append(palabra)
+    print(lista)
+
 while (resp!= '2'):	
 	print("\n**  Menú  ***")
 	print("1.- Imprimir lista ")	
 	print("2.- Salir")
 	resp = input("Opcion: ")
 	if (resp == '1'):
-		pass
+		lista()
 	elif (resp == '2'):
 		print("Adios...")
 	else:
