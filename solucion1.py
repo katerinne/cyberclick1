d3ec35e60c373319335a7d06fb6c935be85d0bef
Belkis <katerinne65@gmail.com>
2022-04-15 17:27:59 +0200

    Documentacion

--- solucion1.py
+++ solucion1.py
@@ -1,19 +1,23 @@
-resp = "0"
+#Este programa genera un listado de numero, 
+# a los multiples de 3, 5 y ambos le agrega la palabra cyber, click y cyberclick respectimante
+# Tiene de salida la lista
 
-def lista():  
+resp = "0"  # variable para el ciclo del menú
+
+def lista(): # Metodo que genera la lista continua y verifica los multiples
     lista = []      
     for i in range (1,101):          
         palabra = ""
-        if( i % 3 == 0):             
+        if( i % 3 == 0): 
             palabra='cyber'
-        if( i % 5 == 0):
+        if( i % 5 == 0): 
             palabra+='click'                       
-        if(palabra == ""):
+        if(palabra == ""): 
             palabra = i
-        lista.append(palabra)
-    print(lista)
+        lista.append(palabra) #agrega a la lista
+    print(lista) 
 
-while (resp!= '2'):	
+while (resp!= '2'):	# Menú
 	print("\n**  Menú  ***")
 	print("1.- Imprimir lista ")	
 	print("2.- Salir")
